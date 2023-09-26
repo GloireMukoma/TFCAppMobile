ab0b6ef81389f14d5b884c65a4994983fd9dd2d3
GloireMukoma <gloiremkabuya@gmail.com>
2023-09-26 21:31:29 +0200

    Creation d'un ecran de chargement et de la barre de navigation

---
+++ app/src/main/java/com/example/projettfc/SpashScreenActivity.kt
@@ -0,0 +1,23 @@
+package com.example.projettfc
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SpashScreenActivity : AppCompatActivity() {
+    private val SPLASH_TIME: Long = 3000
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        supportActionBar?.hide() // Masquer l'actionbar
+
+        setContentView(R.layout.activity_spash_screen)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            // Ferme l'activité d'écran de chargement
+            finish()
+        }, SPLASH_TIME)
+
+    }
+}
